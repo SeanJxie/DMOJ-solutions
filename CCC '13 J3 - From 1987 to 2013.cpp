09601e2ed4df3846dd287a0fda79da38662fb293
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2019-10-24 17:12:08 -0400

    Update CCC '13 J3 - From 1987 to 2013.cpp

--- CCC '13 J3 - From 1987 to 2013.cpp
+++ CCC '13 J3 - From 1987 to 2013.cpp
@@ -1,3 +1,18 @@
+/*
+Canadian Computing Competition: 2013 Stage 1, Junior #3, Senior #1
+You might be surprised to know that 2013 is the first year since 1987 with distinct digits. 
+The years 2013, 2015, 2016, 2017, 2018, 2019 each have distinct digits. 2012 does not have distinct digits, 
+since the digit 2 is repeated.
+
+Given a year, what is the next year with distinct digits?
+
+Input Specification
+The input consists of one integer Y (0≤Y≤10000), representing the starting year.
+
+Output Specification
+The output will be the single integer D, which is the next year after Y with distinct digits.
+*/
+
 #include <iostream>
 #include <string>
 
