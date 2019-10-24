ec07b0e5c0a57e2e801e0462ad1dd7537c3a2d45
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2019-10-24 15:47:26 -0400

    Add files via upload

---
+++ Too Simple.py
@@ -0,0 +1,33 @@
+"""
+FatalEagle is sitting in his computer science class, on his laptop. Clearly, he is not paying attention in this
+situation. However, he gets called on by his computer science teacher to do some problems. Since his computer science
+ teacher did not expect much from FatalEagle, he only needs to output the string Hello, World!. However, simple for you
+ and I may not be simple for FatalEagle, so please help him!
+
+FatalEagle's teacher is clever, and is able to determine that FatalEagle didn't write a solution himself if it uses
+advanced concepts like strings or numbers. Since you don't want to get caught helping FatalEagle, you must make your
+code look like his!
+
+Input
+There is none.
+"""
+
+# I want die.
+one = len([[]])
+five = len([[], [], [], [], []])
+ten = five * (one + one)
+
+seventy_two = ten * (five + one + one) + one + one
+one_hundred_one = ten ** (one + one) + one
+one_hundred_eight = one_hundred_one + ten - one - one - one
+one_hundred_eleven = one_hundred_one + ten
+forty_four = ten * (five - one) + five - one
+thirty_two = ten * (one + one + one) + one + one
+eighty_seven = ten * (five + one + one + one) + five + one + one
+one_hundred_fourteen = ten ** (one + one) + ten + five - one
+one_hundred = ten ** (one + one)
+thirty_three = thirty_two + one
+
+print(chr(seventy_two) + chr(one_hundred_one) + chr(one_hundred_eight) + chr(one_hundred_eight) + chr(
+    one_hundred_eleven) + chr(forty_four) + chr(thirty_two) + chr(eighty_seven) + chr(one_hundred_eleven) +
+      chr(one_hundred_fourteen) + chr(one_hundred_eight) + chr(one_hundred) + chr(thirty_three))
