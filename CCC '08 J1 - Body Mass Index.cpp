6cda7b88b137337dd07533db52c2c343fe964bb2
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2019-11-06 14:16:53 -0500

    Update CCC '08 J1 - Body Mass Index.cpp

--- CCC '08 J1 - Body Mass Index.cpp
+++ CCC '08 J1 - Body Mass Index.cpp
@@ -1,15 +1,19 @@
 /*
-The Body Mass Index (BMI) is one of the calculations used by doctors to assess an adult's health. The doctor measures the patient's height (in metres) and weight (in kilograms), then calculates the BMI using the formula:
+The Body Mass Index (BMI) is one of the calculations used by doctors to 
+assess an adult's health. The doctor measures the patient's height 
+(in metres) and weight (in kilograms), then calculates the BMI using the formula:
 
 BMI=weight/height×height
 
-Write a program which takes the patient's weight and height as input, calculates the BMI, and displays the corresponding message from the table below.
+Write a program which takes the patient's weight and height as input, 
+calculates the BMI, and displays the corresponding message from the table below.
 
 BMI Category	Message
 More than 25	Overweight
 Between 18.5 and 25.0 (inclusive)	Normal weight
 Less than 18.5	Underweight
 */
+
 #include <bits/stdc++.h>
 
 using namespace std;
