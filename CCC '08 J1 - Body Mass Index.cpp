ef83aabc319830a49170b7be081746ee051419a6
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2019-11-06 14:16:13 -0500

    Update CCC '08 J1 - Body Mass Index.cpp

--- CCC '08 J1 - Body Mass Index.cpp
+++ CCC '08 J1 - Body Mass Index.cpp
@@ -1,7 +1,7 @@
 /*
 The Body Mass Index (BMI) is one of the calculations used by doctors to assess an adult's health. The doctor measures the patient's height (in metres) and weight (in kilograms), then calculates the BMI using the formula:
 
-BMI=weight/height×height
+BMI=weight/heightÃ—height
 
 Write a program which takes the patient's weight and height as input, calculates the BMI, and displays the corresponding message from the table below.
 
@@ -10,20 +10,22 @@ More than 25	Overweight
 Between 18.5 and 25.0 (inclusive)	Normal weight
 Less than 18.5	Underweight
 */
-
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    
 	float w, h, bmi;
 	cin >> w >> h;
-	bmi = w / w * h;
+	bmi = w / (h * h);
 
 	if (bmi > 25) {
 		cout << "Overweight";
 	}
-	if (bmi < 18.5) {
+	else if (bmi < 18.5) {
 		cout << "Underweight";
 	}
 	else {
